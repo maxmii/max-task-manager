05b1c9b446964cc8612fab2f4d581f7d39bedc5d
maxm <maxmi@pop-os.localdomain>
2022-04-30 23:29:47 +0100

    Remove port

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const taskRouter = require('./routers/task')
 const bytes = require('bytes')
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 
 const multer = require('multer')
 const upload = multer({
