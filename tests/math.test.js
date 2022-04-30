2079bfc193f304f40dfdbce4516d6a9fd1184156
maxmii <morrm046@gmail.com>
2022-04-30 23:58:40 +0100

    add test directory

--- tests/math.test.js
+++ tests/math.test.js
@@ -2,6 +2,10 @@ test('Hello World', () => {
 
 })
 
+<<<<<<< HEAD
 tset('This should fail', () => {
+=======
+test('This should fail', () => {
+>>>>>>> 6f3cd30 (add test directory)
     throw new Error('Failure')
 })
